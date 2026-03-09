0ed9e157208175b55fd677ccf61d1cffe96cbf83
davinjmeema405-cmyk <davinjmeema405@gmail.com>
2026-03-09 08:53:43 +0200

    Update axivaraCore.js

--- src/core/axivaraCore.js
+++ src/core/axivaraCore.js
@@ -1,10 +1,56 @@
+import fs from "fs";
 import { addMemory } from "../modules/memory.js";
-import { lookupKnowledge } from "../modules/knowledge.js";
 import { learn } from "../modules/learning.js";
-import { securityCheck } from "../modules/security.js";if(!securityCheck(input)){
- return "Blocked by security policy.";
-}const learning = learn(input,this.config.knowledgeFile);
+import { securityCheck } from "../modules/security.js";
 
-if(learning){
- return learning;
+export class AxivaraCore {
+
+ constructor(config){
+
+  this.config = config;
+
+  this.knowledge = JSON.parse(
+   fs.readFileSync(this.config.knowledgeFile)
+  );
+
+ }
+
+ start(){
+  console.log("Axivara started.");
+ }
+
+ process(input){
+
+  if(!securityCheck(input)){
+   return "Blocked by security policy.";
+  }
+
+  const learning = learn(input,this.config.knowledgeFile);
+
+  if(learning){
+
+   this.knowledge = JSON.parse(
+    fs.readFileSync(this.config.knowledgeFile)
+   );
+
+   return learning;
+
+  }
+
+  const lower = input.toLowerCase();
+
+  let response;
+
+  if(this.knowledge[lower]){
+   response = this.knowledge[lower];
+  }else{
+   response = "I do not know that yet.";
+  }
+
+  addMemory(this.config.memoryFile,input,response);
+
+  return response;
+
+ }
+
 }
