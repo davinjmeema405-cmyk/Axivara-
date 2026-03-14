6e1cc8fdf2266aa5eac9680e363e1d19b88e6375
davinjmeema405-cmyk <davinjmeema405@gmail.com>
2026-03-14 07:54:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,29 @@ import express from "express";
 import { AxivaraCore } from "./src/core/axivaraCore.js";
 import { config } from "./config/config.js";
 
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const uri = "mongodb+srv://davinjmeema405_db_user:Axivara_secure_founder-admin@cluster0.zxjtizy.mongodb.net/axivara?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function connectDB() {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+  }
+}
+
+connectDB();
+
 const app = express();
 app.use(express.json());
 
